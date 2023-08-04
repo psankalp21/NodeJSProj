93837a229e3930ef2caf46e901f06a1d815e5720
psankalp21 <prem.sankalp@appinventiv.com>
2023-08-04 15:00:40 +0530

    changes made in port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
-const port = 3000;
+const port = 4000; //port change from 3000 to 4000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
